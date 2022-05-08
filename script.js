5b5d51de23d3f9801ecb22c33a26ad5bc625852f
Luigi Jet <103647877+Luigijet@users.noreply.github.com>
2022-05-08 18:27:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if(this.scrollY > 20){
+			$('.navbar').addClass('sticky');
+		}
+		else{
+			$('.navbar').removeClass('sticky');
+		}
+	})
+});
+
 $('nav ul li a').click(function(){
     $('html').css('scrollBehavior', "smooth");
 });
